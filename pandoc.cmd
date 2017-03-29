ea656a7fb9a06bacf9613de7f0dea75ab33afc63
Valery Dachev <vdachev@david.bg>
2017-03-29 12:22:00 +0300

    Добавени са скриптове за генериране на документация в DOCX и HTML формат с Pandoc

---
+++ pandoc.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+pandoc -s "README.md" -o "README.docx"
+
+pandoc -s "README.md" -o "README.html"
+
+pandoc -s "Техническа спецификация.md" -o "Техническа спецификация.docx"
+
+pandoc -s "Техническа спецификация.md" -o "Техническа спецификация.html"
+
