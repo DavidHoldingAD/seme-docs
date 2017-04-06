46276ccf3da4ff8692376d29325c42c29574fe0d
Valery Dachev <vdachev@david.bg>
2017-04-06 11:18:58 +0300

    Коригирани скриптове за генериране на документацията в DOCX и HTML формат

--- pandoc.cmd
+++ pandoc.cmd
@@ -1,13 +1,11 @@
+﻿@ 2> nul
 @echo off
 
-pandoc -s "README.md" -o "README.docx"
+chcp 65001
 
-pandoc -s "README.md" -o "README.html"
-
-pandoc -s "Техническа спецификация.md" -o "Техническа спецификация.docx"
-
-pandoc -s "Техническа спецификация.md" -o "Техническа спецификация.html"
-
-pandoc -s "Издаване на транспортен сертификат.md" -o "Издаване на транспортен сертификат.docx"
-
-pandoc -s "Издаване на транспортен сертификат.md" -o "Издаване на транспортен сертификат.html"
+pandoc.exe -s "README.md" -o "README.docx"
+pandoc.exe -s "README.md" -o "README.html"
+pandoc.exe -s "Техническа спецификация.md" -o "Техническа спецификация.docx"
+pandoc.exe -s "Техническа спецификация.md" -o "Техническа спецификация.html"
+pandoc.exe -s "Издаване на транспортен сертификат.md" -o "Издаване на транспортен сертификат.docx"
+pandoc.exe -s "Издаване на транспортен сертификат.md" -o "Издаване на транспортен сертификат.html"
