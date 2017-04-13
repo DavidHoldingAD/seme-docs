cf0002cf00c43c84e709f7647aa769851302ed3d
Valery Dachev <vdachev@david.bg>
2017-04-13 08:27:44 +0300

    Използване на PowerShell вместо CMD за генериране на документация под Windows

---
+++ pandoc.ps1
@@ -0,0 +1,16 @@
+﻿#requires -Version 4.0
+
+[CmdletBinding()]
+param (
+)
+
+$FilesMD = Get-ChildItem -Path $PSScriptRoot -Filter "*.md"
+foreach ($FileMD in $FilesMD) {
+	$FileDOCX = "${$FileMD.Name}.docx"
+	Write-Host -Object "Converting `"$FileMD`" to DOCX..."
+	pandoc.exe -s "$FileMD" -o "$FileDOCX"
+
+	$FileHTML = "${$FileMD.Name}.html"
+	Write-Host -Object "Converting `"$FileMD`" to HTML..."
+	pandoc.exe -s "$FileMD" -o "$FileDOCX"
+}
