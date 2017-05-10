a613229584f3346e22159c67b8d85dd6ffa1a35d
Valery Dachev <vdachev@david.bg>
2017-05-10 18:26:22 +0300

    Преименуване на deployment скриптовете и добавяне на директориите към изхода им

---
+++ deploy.ps1
@@ -0,0 +1,31 @@
+﻿#requires -Version 4.0
+
+[CmdletBinding()]
+param (
+)
+
+$WorkingDir = $PSScriptRoot
+$DeployDir = Join-Path -Path $WorkingDir -ChildPath ".deploy"
+
+if (Test-Path -Path $DeployDir) {
+	Remove-Item -Path $DeployDir -Recurse
+}
+
+New-Item -Path $DeployDir -ItemType Directory
+
+$FilesMD = Get-ChildItem -Path $WorkingDir -Filter "*.md"
+foreach ($FileMD in $FilesMD) {
+	Write-Host -Object "Converting `"$FileMD`" to DOCX..."
+	$FileDOCX = Join-Path -Path $DeployDir -ChildPath "$($FileMD.BaseName).docx"
+	pandoc.exe -f markdown_github+hard_line_breaks -s "$FileMD" -o "$FileDOCX" -t docx
+
+	Write-Host -Object "Converting `"$FileMD`" to HTML..."
+	$FileHTML = Join-Path -Path $DeployDir -ChildPath  "$($FileMD.BaseName).html"
+	pandoc.exe -f markdown_github+hard_line_breaks -s "$FileMD" -o "$FileHTML" -t html
+}
+
+$Directories = Get-ChildItem -Path $WorkingDir -Exclude ".*" | ?{ $_.PSIsContainer }
+foreach ($Directory in $Directories) {
+	Write-Host -Object "Copying directory `"$Directory`"..."
+	Copy-Item -Path $Directory -Destination $DeployDir -Recurse
+}
--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@
 # http://pandoc.org/installing.html
 
 WORKING_DIR=`dirname $0`
-OUTPUT_DIR="$WORKING_DIR/.output"
+DEPLOY_DIR="$WORKING_DIR/.deploy"
 
-if [ -e "$OUTPUT_DIR" ]; then
-	rm -rf "$OUTPUT_DIR"
+if [ -e "$DEPLOY_DIR" ]; then
+	rm -rf "$DEPLOY_DIR"
 fi
 
-mkdir -p "$OUTPUT_DIR"
+mkdir -p "$DEPLOY_DIR"
 
 for FILE_MD in "$WORKING_DIR"/*.md; do
 	echo "Converting \"$FILE_MD\" to DOCX..."
-	FILE_DOCX=$OUTPUT_DIR/${FILE_MD%.*}.docx
+	FILE_DOCX=$DEPLOY_DIR/${FILE_MD%.*}.docx
 	pandoc -f markdown_github+hard_line_breaks -s "$FILE_MD" -o "$FILE_DOCX" -t docx
 
 	echo "Converting \"$FILE_MD\" to HTML..."
-	FILE_HTML=$OUTPUT_DIR/${FILE_MD%.*}.html
+	FILE_HTML=$DEPLOY_DIR/${FILE_MD%.*}.html
 	pandoc -f markdown_github+hard_line_breaks -s "$FILE_MD" -o "$FILE_HTML" -t html
 done
